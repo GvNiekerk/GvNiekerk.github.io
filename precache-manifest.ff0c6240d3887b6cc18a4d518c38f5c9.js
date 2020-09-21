6814fefdc87d8c330c29825c1bc07f2921689f02
GvNiekerk <54552369+GvNiekerk@users.noreply.github.com>
2020-09-21 17:02:25 +0200

    Add files via upload

---
+++ precache-manifest.ff0c6240d3887b6cc18a4d518c38f5c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8af2da9d8148aacd0b688102b24c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a56dab5645abf972b65",
+    "url": "/static/css/main.317a4113.chunk.css"
+  },
+  {
+    "revision": "569ce41c508b003eb7c6",
+    "url": "/static/js/2.8dd1f869.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8dd1f869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a56dab5645abf972b65",
+    "url": "/static/js/main.3ec9a39a.chunk.js"
+  },
+  {
+    "revision": "abd8b9d6a9bafb4ba6ce",
+    "url": "/static/js/runtime-main.bf1f4d57.js"
+  },
+  {
+    "revision": "bbee6178f3c643a8dfaabfbf8d635316",
+    "url": "/static/media/GerhardLogo.bbee6178.jpg"
+  },
+  {
+    "revision": "b664cbb2fa3c2c8489094fa7417b5afb",
+    "url": "/static/media/Logo.b664cbb2.png"
+  }
+]);
